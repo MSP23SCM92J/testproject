154f1d216d6f4003126432d709b8ab4a987a7c89
mohitjadhav1 <mjadhav4@hawk.iit.edu>
2023-04-18 18:29:11 -0500

    update

--- include/write.h
+++ include/write.h
@@ -1,8 +1,7 @@
 #include<iostream>
-#include<hdf5.h>
 
 // Write data to H5 dataset
-int writeToDataset(const char* FILE_NAME, const char* DATASET_NAME, const char* H5FILE_NAME, hsize_t CHUNK_SIZE);
+int writeToDataset(const char* FILE_NAME, const char* DATASET_NAME, const char* H5FILE_NAME, int64_t CHUNK_SIZE);
 
 // Append to existing dataset
 int appendToDataset(const char* FILE_NAME, const char* DATASET_NAME, const char* H5FILE_NAME);
